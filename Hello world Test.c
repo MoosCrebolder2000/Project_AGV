bd49bcd2786f155a702acc0d7711666f860ccfcf
MoosCrebolder2000 <105500202+MoosCrebolder2000@users.noreply.github.com>
2022-06-17 15:58:54 +0200

    Update Hello world Test.c

--- Hello world Test.c
+++ Hello world Test.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+#include <util/delay.h>
+
 int main() 
 {
    // idereen weet wel hoe een printf statement werkt neem ik aan
    printf("Hello, World! \n");
    printf("Groetjes Jurriaan katsman");
    printf("Steijn is hier ook geweest");
-printf("Have no fear, Mohammed is hier");
+   printf("Moos hier");
+   printf("Have no fear, Mohammed is hier");
+   
+   _delay_ms(1000);
+   
+  
 
    return 0;
 }
